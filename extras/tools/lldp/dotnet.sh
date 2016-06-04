a55a4a59c05993a159b1f1f659f3a42d9569ab29
Kristian Lyngstol <kly@kly.no>
2016-06-04 19:42:59 +0200

    dotnet: use a folder for output.

--- extras/tools/lldp/dotnet.sh
+++ extras/tools/lldp/dotnet.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -xe
 DATE="$(date --iso-8601=second | sed s/:/./g)"
-JSON=lolwhat-${DATE}.json
+OUT=out/
+mkdir -p $OUT
+JSON=${OUT}lolwhat-${DATE}.json
 ./lolwhat.pl $* > ${JSON}
-./draw-neighbors.pl < ${JSON} | dot -Tpng > lolwhat-${DATE}.png
-./draw-neighbors.pl full < ${JSON} | dot -Tpng > lolwhat-${DATE}-full.png
-echo File name: lolwhat-${DATE}*png
+./draw-neighbors.pl < ${JSON} | dot -Tpng > ${OUT}lolwhat-${DATE}.png
+./draw-neighbors.pl full < ${JSON} | dot -Tpng > ${OUT}lolwhat-${DATE}-full.png
+echo File name: ${OUT}lolwhat-${DATE}*png

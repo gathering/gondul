6cf76dedac65bf785fef8439e8fdeb2ad4130291
S1LV3R <me@s1lv3r.codes>
2026-03-28 18:50:14 +0100

    Add Slack webhook notifications (#250)

---
+++ api/app/core/slack_webhook.py
@@ -0,0 +1,23 @@
+import logging
+
+import validators
+from slack_sdk import WebhookClient
+
+from app.core.config import settings
+from app.models.oplog import OplogBase
+
+log = logging.getLogger(__name__)
+
+client: WebhookClient
+if settings.SLACK_WEBHOOK_URI and validators.url(settings.SLACK_WEBHOOK_URI):
+  client = WebhookClient(settings.SLACK_WEBHOOK_URI)
+
+async def send_oplog_notification(oplog: OplogBase):
+  if not validators.url(settings.SLACK_WEBHOOK_URI):
+    return
+
+  message = f"{oplog.time} {oplog.systems} [{oplog.username}] {oplog.message}"
+  try:
+    client.send(text=message)
+  except Exception as e:
+    log.exception(e)

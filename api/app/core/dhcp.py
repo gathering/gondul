bbb379b9a32f44611b8e7db96cf0b882c265ca5e
theS1LV3R <me@s1lv3r.codes>
2026-03-29 19:03:47 +0200

    api: Add KeaDHCPServer.get_details
    Returns data expected for nms map

--- api/app/core/dhcp.py
+++ api/app/core/dhcp.py
@@ -1,3 +1,6 @@
+import logging
+import time
+
 from pyisckea import Kea
 
 from app.core.config import settings
@@ -7,33 +10,108 @@ URIS = {
     "v6": settings.KEA_DHCP6_URI if settings.KEA_DHCP6_URI else ""
 }
 
+logger = logging.getLogger(__name__)
+
 
 class KeaDHCPServer():
     _v4_client = Kea(URIS["v4"])
     _v6_client = Kea(URIS["v6"])
 
-    def getv4leases(self):
+    def get_v4_leases(self):
         return self._v4_client.dhcp4.lease4_get_all()
 
-    def getv6leases(self):
+    def get_v6_leases(self):
         return self._v6_client.dhcp6.lease6_get_all()
 
+    def get_subnet_ids(self):
+        """
+        Returns subnet IDs for both IPv4 and IPv6 subnets
+        """
+        v4 = self._v4_client.dhcp4.subnet4_list()
+        v6 = self._v4_client.dhcp6.subnet6_list()
+        return [net.id for net in v4+v6 if net.id]
+
     def get_summary(self):
-        v4 = self.getv4leases()
-        v6 = self.getv6leases()
+        # In frontend: Saves to nmsdata.dhcpsummary, see nms-dhcp.js
+        v4 = self.get_v4_leases()
+        v6 = self.get_v6_leases()
 
         unique_v4 = len(set([lease.ip_address for lease in v4]))
         unique_v6 = len(set([lease.ip_address for lease in v6]))
 
-        # Weird format because this is what is expected by existing gui
-        return {"dhcp": {4: unique_v4, 6: unique_v6} }
+        # Return object should match format expected by frontend:
+        # {
+        #   "dhcp": {
+        #     4: unique ipv4 leases (int),
+        #     6: unique ipv6 leases (int),
+        #   }
+        # }
+        return {"dhcp": {4: unique_v4, 6: unique_v6}}
 
     def get_details(self):
+        # In frontend: Saves to nmsdata.dhcp, see nms-map-handlers.js, function dhcpUpdater and dhcpInfo
+        v4_leases = [lease for lease in self.get_v4_leases()
+                     if lease.subnet_id]
+        v6_leases = [lease for lease in self.get_v6_leases()
+                     if lease.subnet_id]
+        subnet_ids = self.get_subnet_ids()
+
+        # When this data collection has occurred
+        now: int = round(time.time())
+
+        # Key: Subnet ID as defined by Kea
+        # Value: Amount of leases belonging to that subnet
+        subnet_lease_counts: dict[int, int] = {}
+
+        # Key: Subnet ID as defined by Kea
+        # Value: Unix timestamp to the last time this was updated
+        # Note: A subnet not being defined as a key in here means there is no
+        #       available leases, and is intentional. Do not initialize to 0.
+        last_lease_refresh_v4: dict[int, int] = {}
+        last_lease_refresh_v6: dict[int, int] = {}
+
+        # Initialize so even subnets without leases return _some_ data
+        for subnet_id in subnet_ids:
+            subnet_lease_counts[subnet_id] = 0
+
+        # Add data for subnet counts
+        for lease in v4_leases+v6_leases:
+            if not lease.subnet_id:
+                continue
+            subnet_lease_counts[lease.subnet_id] += 1
+
+        # Add data for last lease refresh (subnet "staleness")
+        for lease in v4_leases:
+            if not lease.subnet_id or not lease.cltt:
+                continue
+            last_lease_refresh_v4[lease.subnet_id] = max(
+                lease.cltt,
+                last_lease_refresh_v4[lease.subnet_id] or 0
+            )
+        for lease in v6_leases:
+            if not lease.subnet_id or not lease.cltt:
+                continue
+            last_lease_refresh_v6[lease.subnet_id] = max(
+                lease.cltt,
+                last_lease_refresh_v6[lease.subnet_id] or 0
+            )
+
+        # Return object should match format expected by frontend:
         # {
-        #   time: collection timestamp?
-        #   dhcp4: number of ipv4 ips on a vlan?
-        #   dhcp6: number of ipv6 ips on a vlan?
+        #   time: collection timestamp (int)
+        #   dhcp4: object, key=[subnet_id (int)], value=last dhcp refresh timestamp (int)
+        #   dhcp6: object, key=[subnet_id (int)], value=last dhcp refresh timestamp (int)
+        #   networks: {
+        #     [subnet_id (int)]: {
+        #       clients: leases per subnet (int)
+        #     }
+        #   }
         # }
-        pass
+        return {
+            "time": now,
+            "dhcpv4": last_lease_refresh_v4,
+            "dhcpv6": last_lease_refresh_v4,
+            "networks": subnet_lease_counts
+        }
 
 dhcp = KeaDHCPServer()

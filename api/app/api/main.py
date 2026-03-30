e0534f6998da854c97c4f153d254a19ec7a23525
theS1LV3R <me@s1lv3r.codes>
2026-03-30 13:10:47 +0200

    api: actually use dhcp

--- api/app/api/main.py
+++ api/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import utils, public, prometheus, oplog, read, devices
+from app.api.routes import dhcp, utils, public, prometheus, oplog, read, devices
 
 api_router = APIRouter()
 api_router.include_router(utils.router)
@@ -9,3 +9,4 @@ api_router.include_router(prometheus.router)
 api_router.include_router(oplog.router)
 api_router.include_router(read.router)
 api_router.include_router(devices.router)
+api_router.include_router(dhcp.router)

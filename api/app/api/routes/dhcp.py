4aa73439f371a2d1007fab73ed840be59679eab4
theS1LV3R <me@s1lv3r.codes>
2026-03-30 10:59:40 +0200

    api: Implement actual api endpoints

--- api/app/api/routes/dhcp.py
+++ api/app/api/routes/dhcp.py
@@ -1,12 +1,18 @@
-from fastapi import APIRouter
+from hashlib import md5
 
+from fastapi import APIRouter, Depends, Request, Response
+
+from app.api.deps import get_cache
+from app.core.dhcp import dhcp
 
 router = APIRouter(prefix="/v2/dhcp", tags=["dhcp"])
 
+
 @router.get("/")
 async def get_details():
-  pass
+    return dhcp.get_details()
+
 
 @router.get("/summary")
 async def get_summary():
-  pass
+    return dhcp.get_summary()

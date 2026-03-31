66593eda59640e21d47a31f740491906196333c5
theS1LV3R <me@s1lv3r.codes>
2026-03-31 22:55:06 +0200

    api: logic fixes, comments, overall readability

--- api/app/core/dhcp.py
+++ api/app/core/dhcp.py
@@ -13,12 +13,14 @@ URIS = {
     "v6": settings.KEA_DHCP6_API_URI if settings.KEA_DHCP6_API_URI else ""
 }
 
+vlan_id = int
+subnet_id = int
 
 class DHCPSubnetDetails():
     clients: int
     """Leases"""
 
-    def __init__(self, clients: int) -> None:
+    def __init__(self, clients: int = 0) -> None:
         self.clients = clients
 
 
@@ -27,30 +29,31 @@ class DHCPSummaryResponse():
     dhcp: dict[int, int]
     """Has two keys `4` and `6`, each having the amount of unique IP leases for the respective IP version"""
 
-    def __init__(self, dhcp: dict[int, int]) -> None:
+    def __init__(self, dhcp: dict[int, int] = {}) -> None:
         self.dhcp = dhcp
 
 
 class DHCPDetailsResponse():
     # Weird field namings and format is because this is what is expected by gondul
-    time: int
+    time: int = round(time.time())
     """data collection unix timestamp"""
 
-    dhcp4: dict[int, int]
-    """key=[subnet_id], value=last dhcp refresh unix timestamp"""
-    dhcp6: dict[int, int]
-    """key=[subnet_id], value=last dhcp refresh unix timestamp"""
+    dhcp4: dict[vlan_id, int] = {}
+    """key=[vlan_id], value=last dhcp refresh unix timestamp"""
+    dhcp6: dict[vlan_id, int] = {}
+    """key=[vlan_id], value=last dhcp refresh unix timestamp"""
 
-    networks: dict[int, DHCPSubnetDetails]
-    """key=[subnet_id], value={ clients: leases per subnet }"""
+    networks: dict[int, DHCPSubnetDetails] = {}
+    """key=[vlan_id], value={ clients: leases per VLAN }"""
 
 
 class DummyDHCPServer():
-    def get_summary(self):
-        return {"dhcp": {}}
+    def get_summary(self) -> DHCPSummaryResponse:
+        return DHCPSummaryResponse({})
 
-    def get_details(self):
-        return {}
+    def get_details(self) -> DHCPDetailsResponse:
+        response = DHCPDetailsResponse()
+        return response
 
 
 class KeaDHCPServer():
@@ -62,43 +65,47 @@ class KeaDHCPServer():
         self._v4_client = Kea(URIS["v4"])
         self._v6_client = Kea(URIS["v6"])
 
-    def _get_v4_leases(self):
+    def _get_v4_leases(self) -> list[Lease4]:
         leases = self._v4_client.dhcp4.lease4_get_all()
         self._logger.debug(leases)
         return leases
 
-    def _get_v6_leases(self):
+    def _get_v6_leases(self) -> list[Lease6]:
         leases = self._v6_client.dhcp6.lease6_get_all()
         self._logger.debug(leases)
         return leases
 
-    def _get_subnet_vlan_mappings(self) -> dict[int, int]:
+    def _get_subnet_vlan_mappings(self) -> dict[subnet_id, vlan_id]:
         """
-        Returns subnet mappings from Netbox ID (returned by Kea)
+        Returns mappings from Netbox ID (used for subnets in and returned by Kea)
         to VLAN ID (expected by gondul).
 
-        Key: Netbox ID; Value: VLAN ID
+        Key: Netbox (Subnet) ID; Value: VLAN ID
         """
-        subnets = [
+        subnets_v4_v6 = [
+            # Separate lists here because they require separate clients to fetch data (annoyingly)
             [self._v4_client.dhcp4.subnet4_list(), self._v4_client.dhcp4.subnet4_get],
             [self._v6_client.dhcp6.subnet6_list(), self._v6_client.dhcp6.subnet6_get]
         ]
 
-        subnet_map: dict[int, int] = {}
-        for [subnets, get_subnet] in subnets:
-            for net in subnets:
-                if not net.id:
+        subnet_vlan_mapping: dict[subnet_id, vlan_id] = {}
+        for [subnets, get_subnet_details] in subnets_v4_v6:
+            for subnet in subnets:
+                if not subnet.id:
                     continue
-                subnet_data = get_subnet(net.id)
+                subnet_details = get_subnet_details(subnet.id)
 
-                if not subnet_data.user_context or "vlan-id" not in subnet_data.user_context:
+                if not subnet_details.user_context or (
+                    "vlan-id" not in subnet_details.user_context
+                    and "vlan_id" not in subnet_details.user_context
+                ):
                     continue
 
-                subnet_map[net.id] = subnet_data.user_context["vlan-id"]
+                subnet_vlan_mapping[subnet.id] = subnet_details.user_context["vlan-id"] or subnet_details.user_context["vlan_id"]
 
-        return subnet_map
+        return subnet_vlan_mapping
 
-    def get_summary(self):
+    def get_summary(self) -> DHCPSummaryResponse:
         # In frontend: Saves to nmsdata.dhcpsummary, see nms-dhcp.js
         v4 = self._get_v4_leases()
         v6 = self._get_v6_leases()
@@ -114,9 +121,6 @@ class KeaDHCPServer():
         v6_leases = [lease for lease in self._get_v6_leases() if lease.subnet_id]
 
         subnet_vlan_mappings = self._get_subnet_vlan_mappings()
-
-        # When this data collection has occurred
-        now: int = round(time.time())
 
         # Key: VLAN ID (NOT subnet ID from Kea, as this is the Netbox object ID)
         # Value: Object containing the amount of leases belonging to that vlan
@@ -134,18 +138,18 @@ class KeaDHCPServer():
         for _index, (_netbox_id, vlan_id) in enumerate(subnet_vlan_mappings.items()):
             vlan_lease_counts[vlan_id] = DHCPSubnetDetails(0)
 
-        # Add data for subnet counts
         for lease in v4_leases+v6_leases:
             if lease.subnet_id not in subnet_vlan_mappings:
                 continue
-            vlan_lease_counts[subnet_vlan_mappings[lease.subnet_id]].clients += 1
+            vlan_id = subnet_vlan_mappings[lease.subnet_id]
+            vlan_lease_counts[vlan_id].clients += 1
 
         _temp = [
             [v4_leases, last_lease_refresh_v4],
             [v6_leases, last_lease_refresh_v6],
         ]
 
-        # Add data for last lease refresh (subnet "staleness")
+        # Add timestamps for last DHCP lease refresh ("staleness")
         for [leases, last_refreshed] in _temp:
             for lease in leases:
                 if (not lease.cltt or lease.subnet_id not in subnet_vlan_mappings):
@@ -159,7 +163,6 @@ class KeaDHCPServer():
                     last_refreshed[vlan_id] = max(lease.cltt, 0)
 
         response = DHCPDetailsResponse()
-        response.time = now
         response.dhcp4 = last_lease_refresh_v4
         response.dhcp6 = last_lease_refresh_v6
         response.networks = vlan_lease_counts
